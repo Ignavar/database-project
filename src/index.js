36b268b414cad3dfcbb623043aab8e916a0e2aa4
Ignavar <Haroon152018@gmail.com>
2023-12-10 16:13:23 +0500

    Added routing and Employee

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route,} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <BrowserRouter basename="/app">
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/Employee" element={<Employee/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
